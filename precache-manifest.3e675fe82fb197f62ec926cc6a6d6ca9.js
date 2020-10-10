335f6847907572b04667c8cf9aeea542d94be619
Neel9784 <neel9784@gmail.com>
2020-10-10 11:17:52 +0530

    Updates

---
+++ precache-manifest.3e675fe82fb197f62ec926cc6a6d6ca9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b871c7e9698989e8152f8dcd3cac881",
+    "url": "/navakgroup/index.html"
+  },
+  {
+    "revision": "49e9f3e7f613cd69465e",
+    "url": "/navakgroup/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "776983a274f3618a6ec9",
+    "url": "/navakgroup/static/css/main.a7e0adc5.chunk.css"
+  },
+  {
+    "revision": "49e9f3e7f613cd69465e",
+    "url": "/navakgroup/static/js/2.2928dc78.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/navakgroup/static/js/2.2928dc78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776983a274f3618a6ec9",
+    "url": "/navakgroup/static/js/main.c1d08977.chunk.js"
+  },
+  {
+    "revision": "75bfb0d3a138022f8ce9",
+    "url": "/navakgroup/static/js/runtime-main.bec24e78.js"
+  },
+  {
+    "revision": "83cdd4d465ef2a7dbc0bf592cbd0fadc",
+    "url": "/navakgroup/static/media/android.83cdd4d4.png"
+  },
+  {
+    "revision": "555bdf1f6549d1e9546408ee4bf7b080",
+    "url": "/navakgroup/static/media/app.555bdf1f.png"
+  },
+  {
+    "revision": "6611215533011572636a0e9227f05097",
+    "url": "/navakgroup/static/media/digital.66112155.png"
+  },
+  {
+    "revision": "92756e7a0134ef9116f9d6ba6c7a594a",
+    "url": "/navakgroup/static/media/hero-img.92756e7a.png"
+  },
+  {
+    "revision": "44e1babf3a44bffb3a19195d604af377",
+    "url": "/navakgroup/static/media/img1.44e1babf.svg"
+  },
+  {
+    "revision": "32cd5951e96277c3bfb65435851a8b95",
+    "url": "/navakgroup/static/media/marketing.32cd5951.png"
+  },
+  {
+    "revision": "02571116fb35bca90800f0cb517383fc",
+    "url": "/navakgroup/static/media/software.02571116.png"
+  }
+]);
